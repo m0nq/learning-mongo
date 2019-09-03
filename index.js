b8f30dfc12daf110d760f0822c6c1a3a647b7954
Monk Wellington <forgetmenaught@gmail.com>
2019-09-02 20:41:18 -0700

    Configured Hapi to return data correctly

--- index.js
+++ index.js
@@ -1,29 +1,84 @@
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient,
+  Hapi = require('@hapi/hapi');
 
-// Connection URL
 const url = 'mongodb://localhost:27017';
-
-// Database Name
 const dbName = 'learning_mongo';
 
-// find documents
-const findDocuments = (db, callback) => {
-  const toursCollection = db.collection('tours');
-
-  toursCollection.find({'tourPackage': 'Snowboard Cali'}).toArray((err, docs) => {
-    console.log('Found the following records');
-    console.log(docs);
-    callback(docs);
+const init = async collection => {
+  const server = Hapi.server({
+    port: 8080,
+    host: 'localhost'
   });
+
+  server.route([
+    // Get tour list
+    {
+      method: 'GET',
+      path: '/api/tours',
+      config: {json: { space: 2}},
+      handler: (request, h) => {
+        const findObject = {};
+        for (let key in request.query) {
+          findObject[key] = request.query[key];
+        }
+        return collection.find(findObject).toArray();
+      }
+    },
+    // Add new tour
+    {
+      method: 'POST',
+      path: '/api/tours',
+      handler: (request, reply) => {
+        reply('Adding new tour');
+      }
+    },
+    // Get a single tour
+    {
+      method: 'GET',
+      path: '/api/tours/{name}',
+      handler: (request, h) => {
+        return collection.findOne({'tourName': request.params.name});
+      }
+    },
+    // Update a single tour
+    {
+      method: 'PUT',
+      path: '/api/tours/{name}',
+      handler: (request, reply) => {
+        // request.payload variables
+        reply(`Updating ${request.params.name}`);
+      }
+    },
+    // Delete a single tour
+    {
+      method: 'DELETE',
+      path: '/api/tours/{name}',
+      handler: (request, reply) => {
+        reply(`Deleting ${request.params.name}`).code(204);
+      }
+    },
+    // Home page
+    {
+      method: 'GET',
+      path: '/',
+      handler: (request, reply) => {
+        reply('Hello world from Hapi/Mongo example.');
+      }
+    }
+  ]);
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
-// Use connect method to connect to the server
 MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-  console.log('Connected successfully to server');
+  console.log('connected correctly to server');
+  const db = client.db(dbName);
+  let collection = db.collection('tours');
 
-  const learningMongoDB = client.db(dbName);
-
-  findDocuments(learningMongoDB, docs => {
-    client.close();
+  process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
   });
+
+  init(collection);
 });
